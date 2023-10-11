99a3bcefbf5308a092b14ecdc2bec445ba454716
mwairimu <mwairimu@fintech-group.com>
2023-10-11 02:06:47 -0600

    pass method as props in react

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ChildComponent(props) {
   return (
     <div>
-        <button onClick={props.greetHandler}>Greetparent</button>
+        <button onClick={() =>props.greetHandler('child')}>Greetparent</button>
       
     </div>
   )
